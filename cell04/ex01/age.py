ef25a23a6fb7cf935de9f64eb7c107f61d96788a
TSBZ <beemza2288@gmail.com>
2026-02-10 18:35:01 +0700

    ex04

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+age =int(input("Please tell me your age: "))
+print(f"You are currently {age} years old.")
+i = 1
+while i <= 3:
+	print(f"In {i*10} years, you'll be {i*10 + age} years old.")
+	i += 1
---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,4 @@
+import math
+n = input("Give me a number: ")
+roundup = math.ceil(float(n))
+print(roundup)
